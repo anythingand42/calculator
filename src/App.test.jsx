a36032a41e65d4f7473f0dda14874725e556385f
user <user@localhost.localdomain>
2021-04-01 19:31:56 +0300

    add eslint

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders lalala text', () => {
   render(<App />);
-  expect(screen.getByText("lalala")).toBeInTheDocument();
+  expect(screen.getByText('lalala')).toBeInTheDocument();
 });
