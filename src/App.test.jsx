41b2697e2a58c016ae01199c9527474413e74089
user <user@localhost.localdomain>
2021-04-01 18:43:13 +0300

    initial commit

---
+++ src/App.test.jsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders lalala text', () => {
+  render(<App />);
+  expect(screen.getByText("lalala")).toBeInTheDocument();
+});
