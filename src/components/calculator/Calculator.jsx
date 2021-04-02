73345b58d8c2b542fe8ab0ad96e34526f9bcf098
user <user@localhost.localdomain>
2021-04-02 14:17:09 +0300

    extract logic to the useCalculatorLogic hook

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,46 +1,9 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import CalculatorForm from './calculator-form';
 import History from './history';
-
-const ActionEnum = {
-  setOperation: 0,
-  setA: 1,
-  setB: 2,
-  calculate: 3,
-};
-
-const initialState = {
-  operation: '+',
-  A: '',
-  B: '',
-  history: [],
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case ActionEnum.setOperation:
-      return { ...state, operation: action.payload };
-    case ActionEnum.setA:
-      return { ...state, A: action.payload };
-    case ActionEnum.setB:
-      return { ...state, B: action.payload };
-    case ActionEnum.calculate: {
-      return {
-        ...state,
-        history: state.history.concat({
-          A: state.A,
-          B: state.B,
-          operation: state.operation,
-          result: String(parseFloat(state.A) + parseFloat(state.B)),
-        }),
-      };
-    }
-    default:
-      throw new Error(`Unknown action type: ${action.type}`);
-  }
-}
+import useCalculatorLogic from './useCalculatorLogic';
 
 const Box = styled.div`
   max-width: 768px;
@@ -55,7 +18,7 @@ const Box = styled.div`
 `;
 
 function Calculator() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useCalculatorLogic();
   return (
     <Box>
       <CalculatorForm A={state.A} B={state.B} operation={state.operation} dispatch={dispatch} />
---
+++ src/components/calculator/useCalculatorLogic.js
@@ -0,0 +1,43 @@
+import { useReducer } from 'react';
+
+import OperationEnum from '../../math-utils/OperationEnum';
+import operationHandlers from '../../math-utils/operationHandlers';
+import { ActionEnum } from './shared/const';
+
+const initialState = {
+  operation: OperationEnum.sum,
+  A: '',
+  B: '',
+  history: [],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ActionEnum.setOperation:
+      return { ...state, operation: action.payload };
+    case ActionEnum.setA:
+      return { ...state, A: action.payload };
+    case ActionEnum.setB:
+      return { ...state, B: action.payload };
+    case ActionEnum.calculate: {
+      const result = operationHandlers[state.operation](parseFloat(state.A), parseFloat(state.B));
+      return {
+        ...state,
+        history: state.history.concat({
+          A: state.A,
+          B: state.B,
+          operation: state.operation,
+          result: result ? String(result) : 'not found',
+        }),
+      };
+    }
+    default:
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
+}
+
+function useCalculatorLogic() {
+  return useReducer(reducer, initialState);
+}
+
+export default useCalculatorLogic;
