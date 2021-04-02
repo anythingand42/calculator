a2767c477f144967e2921f95b1223ab3f86c0cfd
user <user@localhost.localdomain>
2021-04-02 19:06:28 +0300

    fix App tests

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import 'jest-styled-components';
 
 import App from '../App';
+import OperationEnum from '../math-utils/OperationEnum';
 
-test('renders correctly', () => {
+test('initial state', () => {
   const { container } = render(<App />);
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('main usage', () => {
+  const { container } = render(<App />);
+  const inputA = screen.getByTestId('input-a');
+  const inputB = screen.getByTestId('input-b');
+  const operationSelect = screen.getByTestId('operation-select');
+  const calcButton = screen.getByTestId('calculate');
+
+  const doCalculation = (A, B, operation) => {
+    fireEvent.change(inputA, { target: { value: A } });
+    fireEvent.change(inputB, { target: { value: B } });
+    fireEvent.change(operationSelect, { target: { value: operation } });
+    fireEvent.click(calcButton);
+  };
+
+  doCalculation('2', '4', OperationEnum.sum);
+  doCalculation('20', '4', OperationEnum.divide);
+  doCalculation('297', '13', OperationEnum.mod);
+  doCalculation('34', '900', OperationEnum.maxPrimeBetween);
+
   expect(container.firstChild).toMatchSnapshot();
 });
