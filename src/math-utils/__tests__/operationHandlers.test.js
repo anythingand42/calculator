3fbd2354c8d7d87a84bf7eec33611d7eb6106f1f
user <user@localhost.localdomain>
2021-04-02 16:54:36 +0300

    add tests for divide and maxPrimeNumber

--- src/math-utils/__tests__/operationHandlers.test.js
+++ src/math-utils/__tests__/operationHandlers.test.js
@@ -24,4 +24,34 @@ describe('tests operation handlers', () => {
       expect(operationHandlers[OperationEnum.divide](sample[0], sample[1])).toBe(sample[2]);
     });
   });
+
+  it('tests mod', () => {
+    [
+      [2, 2, 0],
+      [-500, 500, -0],
+      [-501, -500, -1],
+      [6457, 2000, 457],
+      [7, 6, 1],
+      [298, 17, 9],
+    ].forEach((sample) => {
+      expect(operationHandlers[OperationEnum.mod](sample[0], sample[1])).toBe(sample[2]);
+    });
+  });
+
+  it('tests maxPrimeBetween', () => {
+    [
+      [0, 0, null],
+      [0, 1, null],
+      [0, 2, 2],
+      [1.5, 2.5, 2],
+      [900, 3949.834, 3947],
+      [9000, 23, null],
+      [-9999, 2, 2],
+      [-9999, 0, null],
+    ].forEach((sample) => {
+      expect(
+        operationHandlers[OperationEnum.maxPrimeBetween](sample[0], sample[1]),
+      ).toBe(sample[2]);
+    });
+  });
 });
